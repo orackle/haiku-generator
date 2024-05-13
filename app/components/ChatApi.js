4041a5e5befbadfe8c44cb2fca843f9113f28d7c
orackle <ranjana1997@gmail.com>
2024-05-13 11:39:13 -0400

    disguised api key

--- app/components/ChatApi.js
+++ app/components/ChatApi.js
@@ -18,7 +18,7 @@ const ChatApi = () => {
     try {
 
       // console.log(process.env.GEMINI_API_KEY);
-      const response = await axios.post("https://generativelanguage.googleapis.com/v1beta/models/gemini-pro:generateContent?key=AIzaSyCm3KGY5FmojSq8shhXmkLSGjpCG5Bw2kY", {
+      const response = await axios.post(`https://generativelanguage.googleapis.com/v1beta/models/gemini-pro:generateContent?key=${process.env.GEMINI_API_KEY}`, {
         contents: [
           {
             parts: [
